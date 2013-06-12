bcdec0c5fde3553fa7879f0ef8c2ffe48c55ad8b
Gulshan Singh <gulshan@umich.edu>
2013-06-11 22:32:29 -0700

    Create wordsearch on generate and allow user to input size

--- web/grid.dart
+++ web/grid.dart
@@ -65,6 +65,7 @@ class Grid {
   
   void display() {
     DivElement wordsearchDiv = query("#wordsearch");
+    wordsearchDiv.innerHtml = "";
     for (int i = 0; i < dim; i++) {
       for (int j = 0; j < dim; j++) {
         ButtonElement b = new ButtonElement();
--- web/punjabiwordsearch.dart
+++ web/punjabiwordsearch.dart
@@ -1,16 +1,34 @@
 import 'dart:html';
-import 'dart:math';
 import 'grid.dart';
 
+const MIN_DIM = 5;
+const MAX_DIM = 20;
+
 void main() {
+  generate();
+  query('#generate')..onClick.listen((e) => generate());
+}
+
+void generate() {
   List<String> words = getWords();
-  int dim = getDimensions();
-  Grid grid = createGrid(dim, words);
-  grid.display();
+  try {
+    int dim = getDimensions();
+    Grid grid = createGrid(dim, words);
+    grid.display();
+  } on FormatException {
+    print("Dimension not an integer");
+  } catch(e) {
+    print("Invalid integer " + e.toString());
+  }
 }
 
 int getDimensions() {
-  return 10;
+  InputElement input = query("input[name='dimensions']");
+  int val = int.parse(input.value);
+  if (val >= MIN_DIM && val <= MAX_DIM) {
+    return val;
+  }
+  throw val;
 }
 
 List<String> getWords() {
